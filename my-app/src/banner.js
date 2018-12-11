75b26a450c0c62b8fa928f1c3551a0820f8f156c
krishna-kiwi <krishna.kumar@kiwitech.com>
2018-12-11 19:14:40 +0530

    day-02: add component

---
+++ my-app/src/banner.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+
+export default class Banner extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Content/>
+      </div>
+    );
+  }
+}
+
+class Header extends React.Component{
+  render() {
+    var myStyle={
+      fontSize:20,
+      color:'red'
+    }
+    return (
+      <div style={myStyle}>Header Logo</div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  render() {
+    return (
+      <div>
+          <h2>Content</h2>
+          <p>The content text!!!</p>
+      </div>
+    );
+  }
+}
+
+
+
+
