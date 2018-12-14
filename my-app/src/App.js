421f65df8037b1a448cb1ae1903eb3b320a8abe2
krishna-kiwi <krishna.kumar@kiwitech.com>
2018-12-14 17:04:13 +0530

    Using  State and Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Banner from './banner.js';
-import Welcome from './demo.js';
+import Person from './person.js';
+
 
 class App extends React.Component {
   render() {
+  	
     return (
       <div>
-        <Banner/>
-        <Welcome name="krishna"/>
-         <Welcome name="React"/>
+        <Person/>
       </div>
     );
   }
